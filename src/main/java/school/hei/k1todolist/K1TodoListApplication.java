53fa126739787dcc250acd00335b77ca379c48a6
Whipander <mihaja2506@gmail.com>
2025-08-12 11:11:41 +0300

    chore: intial commit

---
+++ src/main/java/school/hei/k1todolist/K1TodoListApplication.java
@@ -0,0 +1,13 @@
+package school.hei.k1todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K1TodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K1TodoListApplication.class, args);
+	}
+
+}
